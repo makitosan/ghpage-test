ce63fa2bd716999ef8e3e71d2636d44899afaffc
Aki <makitosansan@gmail.com>
2018-06-23 20:48:00 +0900

    Update demo

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ghpage-test
 Github Pages test
+
+# DEMO
+
+Visit https://makitosan.github.io/ghpage-test/
--- src/webusb.js
+++ src/webusb.js
@@ -1,1 +1,25 @@
 console.log('this is webusb.js');
+
+const auth = () => {
+    const vendorId = 0x0d28;
+    const productId = 0x0204;
+    navigator.usb.requestDevice({ 'filters': [
+            { 'vendorId': vendorId, 'productId': productId }
+        ]
+    }).then(device => {
+        console.log(device.vendorId);
+        console.log(device.productId);
+        console.log(device.serialNumber);
+        // invoke ivent for serial number
+        var event = new CustomEvent('bitsensei.usb_serial', {'detail': {'serialNumber': device.serialNumber}});
+        document.dispatchEvent(event);
+        // return device.open;
+    }).catch(error => {
+        console.log(error);
+    });
+}
+
+let button = document.getElementById('button');
+button.addEventListener('click', function(event) {
+    auth();
+});
